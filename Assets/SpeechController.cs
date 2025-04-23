ae2f45a9d3c936930a59fbb70d952984c4ce03e7
Corbeau <aurora.griffith@students.mq.edu.au>
2025-04-23 19:35:46 +1000

    created skeleton of new speech bubble style

---
+++ Assets/SpeechController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechController : MonoBehaviour
+{
+    public GameObject SpeechBubbleCollection;
+
+    public SpriteRenderer SizeElement;
+    public SpriteRenderer TypeElement;
+    public SpriteRenderer MilkElement;
+    public SpriteRenderer SweetenerElement;
+
+    public Sprite[] SizeSpriteList;
+    public Sprite[] TypeSpriteList;
+    public Sprite[] MilkSpriteList;
+    public Sprite[] SweetenerSpriteList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
