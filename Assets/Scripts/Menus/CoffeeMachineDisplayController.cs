7c049f30f57b11914b0ba300904c875e10da2ef9
Corbeau <aurora.griffith@students.mq.edu.au>
2025-04-28 14:41:28 +1000

    menu restructuring and new sprites

--- Assets/Scripts/Menus/CoffeeMachineDisplayController.cs
+++ Assets/Scripts/Menus/CoffeeMachineDisplayController.cs
@@ -6,6 +6,7 @@ public class CoffeeMachineDisplayController : MonoBehaviour
 {
     public BlipCounter SuccessBlipCounter;
     public BlipCounter ErrorBlipCounter;
+    public OrderErrorDisplay ErrorDisplay;
 
     // should enum our status
     public bool IsTooManyErrors(){
@@ -19,26 +20,29 @@ public class CoffeeMachineDisplayController : MonoBehaviour
     public void ProcessOrderTransaction( int orderErrorCount ){
         if( orderErrorCount == 0 ){
             this.SuccessBlipCounter.Increase();
+            this.ErrorDisplay.Clear();
         }
         else {
             this.ErrorBlipCounter.Increase();
+            this.ErrorDisplay.Set( orderErrorCount );
         }
     }
 
     public void ResetMachine(){
         this.SuccessBlipCounter.ResetCounter();
         this.ErrorBlipCounter.ResetCounter();
+            this.ErrorDisplay.Clear();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ...
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ...
     }
 }
---
+++ Assets/Scripts/Menus/OrderErrorDisplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderErrorDisplay : MonoBehaviour
+{
+    public SevenSegmentController SevenSegment;
+
+    public void Set(int newValue){
+        this.SevenSegment.SetDisplayDigit(newValue % 10);
+    }
+    public void Clear(){
+        this.SevenSegment.SetDisplayDigit( 0 );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ...
+    }
+}
